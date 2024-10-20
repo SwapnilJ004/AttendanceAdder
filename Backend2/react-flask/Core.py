0d61b5b20983cfa21e13111241ee9a68d6b46605
swapnil0154 <joshiswapnil2004@gmail.com>
2024-10-20 18:46:24 +0530

    CoreCodeFirstWorking

--- Backend2/react-flask/Core.py
+++ Backend2/react-flask/Core.py
@@ -1,6 +1,9 @@
 import cv2
 import layoutparser as lp
 from paddleocr import PaddleOCR, draw_ocr
+import tensorflow as tf
+import numpy as np
+import pandas as pd
 image = cv2.imread("./uploads/Attendance1.jpg")
 image = image[..., ::-1]
 
@@ -32,7 +35,7 @@ image_height = image_cv.shape[0]
 image_width = image_cv.shape[1]
 output = ocr.ocr(image_path)[0]
 
-print(output)
+# print(output)
 
 boxes = [line[0] for line in output]
 texts = [line[1][0] for line in output]
@@ -46,6 +49,106 @@ for box,text in zip(boxes,texts):
 
 cv2.imwrite('detections.jpg', image_boxes)
 
+im = image_cv.copy()
+
 horiz_boxes = []
 vert_boxes = []
 
+for box in boxes:
+  x_h, x_v = 0, int(box[0][0])
+  y_h, y_v = int(box[0][1]), 0
+  width_h, width_v = image_width, int(box[2][0] - box[0][0])
+  height_h, height_v = int(box[2][1] - box[0][1]), image_height
+
+  horiz_boxes.append([x_h, y_h, x_h+width_h, y_h+height_h])
+  vert_boxes.append([x_v, y_v, x_v+width_v, y_v+height_v])
+
+  cv2.rectangle(im, (x_h, y_h), (x_h+width_h, y_h+height_h), (255,0,0), 1)
+  cv2.rectangle(im, (x_v, y_v),(x_v+width_v, y_v+height_v), (0,255,0), 1)
+
+cv2.imwrite('horiz_vert.jpg', im)
+
+#Using tensorflow for lines suppression
+horiz_out = tf.image.non_max_suppression(
+  horiz_boxes,
+  probabilities,
+  max_output_size = 1000,
+  iou_threshold = 0.1,
+  score_threshold = float('-inf'),
+  name = None
+)
+
+horiz_lines = np.sort(np.array(horiz_out))
+
+# print(horiz_lines)
+
+im_nms = image_cv.copy() #Normalized img
+
+for val in horiz_lines:
+  cv2.rectangle(im_nms, (int(horiz_boxes[val][0]), int(horiz_boxes[val][1]), int(horiz_boxes[val][2]), int(horiz_boxes[val][3])), (0,0,255), 1)
+
+vert_out = tf.image.non_max_suppression(
+  vert_boxes,
+  probabilities,
+  max_output_size = 1000,
+  iou_threshold = 0.1,
+  score_threshold = float('-inf'),
+  name = None
+)
+
+vert_lines = np.sort(np.array(vert_out))
+
+# print(vert_lines)
+
+for val in vert_lines:
+  cv2.rectangle(im_nms, (int(vert_boxes[val][0]), int(vert_boxes[val][1]), int(vert_boxes[val][2]), int(vert_boxes[val][3])), (255,0,0), 1)
+
+cv2.imwrite("im_nms.jpg", im_nms)
+
+#Convert to CSV
+out_array = [["" for i in range(len(vert_lines))] for j in range(len(horiz_lines))]
+
+# print(np.array(out_array).shape)
+
+unordered_boxes = []
+
+for i in vert_lines:
+  unordered_boxes.append(vert_boxes[i][0])
+
+ordered_boxes = np.argsort(unordered_boxes)
+print(ordered_boxes)
+
+def intersection(box_1, box_2):
+  return [box_2[0], box_1[1], box_2[2], box_1[3]]
+
+def iou(box_1, box_2):
+  x_1 = max(box_1[0], box_2[0])
+  y_1 = max(box_1[1], box_2[1])
+  x_2 = min(box_1[2], box_2[2])
+  y_2 = min(box_1[3], box_2[3])
+
+  inter = abs(max((x_2 - x_1), 0) * max((y_2 - y_1), 0))
+  if inter == 0:
+    return 0
+  
+  box_1_area = abs((box_1[2] - box_1[0]) * (box_1[3] - box_1[1]))
+  box_2_area = abs((box_2[2] - box_2[0]) * (box_2[3] - box_2[1]))
+
+  print("Union is:", float(box_1_area + box_2_area - inter))
+
+  return inter / float(box_1_area + box_2_area - inter)
+
+
+for i in range(len(horiz_lines)):
+  for j in range(len(vert_lines)):
+    resultant = intersection(horiz_boxes[horiz_lines[i]], vert_boxes[vert_lines[ordered_boxes[j]]])
+    # print(resultant)
+
+    for b in range(len(boxes)):
+      the_box = [boxes[b][0][0], boxes[b][0][1], boxes[b][2][0], boxes[b][2][1]]
+      if(iou(resultant, the_box) > 0.1):
+        out_array[i][j] = texts[b]
+
+# print("Output is:", out_array)
+
+pd.DataFrame(out_array).to_csv("Attendance1.csv")
